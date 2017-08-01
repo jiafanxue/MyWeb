26abac065307b2b9bca58fef7680103597a4573b
xefvan <tutuxsfly@gmail.com>
2017-08-01 23:49:32 +0800

    Add website some config file and wait to add...

---
+++ config.py
@@ -0,0 +1,10 @@
+DEBUG = True
+
+SECRET_KEY = 'secret key'
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:TUXSwuyou2004@localhost:3306/test?charset=utf8'
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+DEBUG_LOG = 'logs/debug.log'
+ERROR_LOG = 'logs/error.log'
